4f7ddbb0c20c96fe7a39cad1caa8b4694b78c3ad
Saubhik Dey <109862202+Saubhik998@users.noreply.github.com>
2025-03-19 15:17:15 +0530

    Delete src directory

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import imageReducer from "./reducers";  // Import the reducer handling image state
-
-// Create the Redux store with imageReducer
-const store = configureStore({
-    reducer: imageReducer,
-});
-
-export default store;
